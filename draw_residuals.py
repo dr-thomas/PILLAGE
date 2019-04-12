2b26516d1b0b576a7b05308c5d06a17bead53156
Thomas Campbell <thomascampbell1@gmail.com>
2019-04-12 15:14:46 -0600

    added first process and some more plots in draw_residuals

--- draw_residuals.py
+++ draw_residuals.py
@@ -1,23 +1,36 @@
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 class event_momentums():
     def __init__(self):
         self.pred_moms = []
         self.true_moms = []
 
-momentum_data = pickle.load(open('./event_momentum_data.sav', 'rb'))
+momentum_data = pickle.load(open('./model_saves/results/event_momentum_data_full.sav', 'rb'))
 
+#TODO: look at predicted momentum distribution and true momentum distribution.  something maybe fishy about pred...
 res_hist = np.array([])
+res_mom_hist = np.array([])
+pred_hist = np.array([])
+true_hist = np.array([])
 for evt in momentum_data:
     for pred_mom in evt.pred_moms:
+        pred_hist = np.append(pred_hist, pred_mom)
+    for true_mom in evt.true_moms:
+        true_hist = np.append(true_hist, true_mom)
+for evt in momentum_data:
+    for pred_mom in evt.pred_moms:
+        if pred_mom > 160. or pred_mom < 70.:
+            continue
         min_residual = 555e10
         for true_mom in evt.true_moms:
-            if abs(pred_mom-true_mom) < min_residual:
+            if abs(pred_mom-true_mom) < abs(min_residual):
                 min_residual = pred_mom-true_mom
         if min_residual < 1e5:
             res_hist = np.append(res_hist, min_residual)
+            res_mom_hist = np.append(res_mom_hist, pred_mom)
 
 plt.figure()
 ax = plt.subplot(111)
@@ -37,6 +50,35 @@ props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 ax.text(0.75, 0.95, textstr, transform=ax.transAxes, fontsize=14,
         verticalalignment='top', bbox=props)
 
-plt.savefig("./residuals_hist.png")
+plt.savefig("./residuals_hist_test.png")
 
+plt.figure()
+ax = plt.subplot(111)
+ax.hist2d(res_mom_hist, res_hist, bins=50)
+ax.set_ylim([-50,50])
+ax.set_xlabel("pred. momentum (MeV/c)")
+ax.set_ylabel("min. residual (MeV/c)")
+plt.savefig("./residuals_2D_hist.png")
+
+plt.figure()
+ax = plt.subplot(131)
+ax.hist(pred_hist, bins=100)
+ax.set_title("predicted")
+ax = plt.subplot(132)
+ax.hist(true_hist, bins=100)
+ax.set_title("true")
+
+smeared_true_hist = np.array([])
+for mm in true_hist:
+    if mm < 70 or mm > 160:
+        continue
+    randx = random.gauss(mu=1,sigma=0.2)
+    smeared_true_hist = np.append(smeared_true_hist, mm*randx)
+ax = plt.subplot(133)
+ax.hist(smeared_true_hist, bins=100)
+ax.set_xlim([0,200])
+ax.set_title("smeared_true")
+
+plt.tight_layout()
+plt.savefig("./momentum_hists.png")
 
---
+++ process.py
@@ -0,0 +1,43 @@
+import csv
+from RANSAC import process_ransac
+import numpy as np
+import pickle
+from sklearn import preprocessing
+
+class event_momentums():
+    def __init__(self):
+        self.pred_moms = []
+        self.true_moms = []
+
+def process_files(in_file_list, batch):
+    momentum_data_out = []
+    mlp_regressor = pickle.load(open('./model_saves/scaled/protons_nn_full_ss.sav', 'rb'))
+    std_scaler = pickle.load(open('./model_saves/scaled/protons_nn_full_ss_std_scaler.sav', 'rb'))
+
+    for ifile, file_path in enumerate(in_file_list):
+        with open(file_path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                evt_moms = event_momentums()
+                n_particles = 0
+                hit_data = []
+                for ifeature, feature in enumerate(row):
+                    if ifeature == 0:
+                        n_particles = int(float(feature))
+                    elif ifeature <= n_particles:
+                        evt_moms.true_moms.append(float(feature)*1000.)
+                    else:
+                        hit_data.append(float(feature))
+
+                clusters = process_ransac.cluster(hit_data)
+
+                for cluster in clusters:
+                    data_X = np.ndarray((1,len(cluster)))
+                    for ii, cc in enumerate(cluster):
+                        data_X[0][ii] = float(cc)
+                    data_X_scaled = std_scaler.transform(data_X)
+                    evt_moms.pred_moms.append(mlp_regressor.predict(data_X_scaled))
+
+    save_str = 'event_momentum_data' + str(batch) + '.sav'
+    pickle.dump(momentum_data_out, open(save_str, 'wb'))
+
